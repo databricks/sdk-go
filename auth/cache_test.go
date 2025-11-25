f404a54f02421452ab42fb481e545798e4daf41f
Ubuntu <renaud.hartert@databricks.com>
2025-11-25 08:16:33 +0000

    Init core modules

---
+++ auth/cache_test.go
@@ -0,0 +1,274 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestNewCachedTokenProvider_noCaching(t *testing.T) {
+	want := &cachedTokenProvider{}
+	got := NewCachedTokenProvider(want, nil)
+	if got != want {
+		t.Errorf("NewCachedTokenProvider() = %v, want %v", got, want)
+	}
+}
+
+func TestNewCachedTokenProvider_default(t *testing.T) {
+	ts := TokenProviderFn(func(_ context.Context) (*Token, error) {
+		return nil, nil
+	})
+
+	got, ok := NewCachedTokenProvider(ts).(*cachedTokenProvider)
+	if !ok {
+		t.Fatalf("NewCachedTokenProvider() = %T, want *cachedTokenProvider", got)
+	}
+
+	if got.staleDuration != defaultStaleDuration {
+		t.Errorf("NewCachedTokenProvider() staleDuration = %v, want %v", got.staleDuration, defaultStaleDuration)
+	}
+	if got.disableAsync != false {
+		t.Errorf("NewCachedTokenProvider() disableAsync = %v, want %v", got.disableAsync, false)
+	}
+	if got.cachedToken != nil {
+		t.Errorf("NewCachedTokenProvider() cachedToken = %v, want nil", got.cachedToken)
+	}
+}
+
+func TestNewCachedTokenProvider_options(t *testing.T) {
+	ts := TokenProviderFn(func(_ context.Context) (*Token, error) {
+		return nil, nil
+	})
+
+	wantDisableAsync := false
+	wantCachedToken := &Token{Expiry: time.Unix(42, 0)}
+
+	opts := []Option{
+		WithAsyncRefresh(!wantDisableAsync),
+		WithCachedToken(wantCachedToken),
+	}
+
+	got, ok := NewCachedTokenProvider(ts, opts...).(*cachedTokenProvider)
+	if !ok {
+		t.Fatalf("NewCachedTokenProvider() = %T, want *cachedTokenProvider", got)
+	}
+
+	if got.disableAsync != wantDisableAsync {
+		t.Errorf("NewCachedTokenProvider(): disableAsync = %v, want %v", got.disableAsync, wantDisableAsync)
+	}
+	if got.cachedToken != wantCachedToken {
+		t.Errorf("NewCachedTokenProvider(): cachedToken = %v, want %v", got.cachedToken, wantCachedToken)
+	}
+}
+
+func TestCachedTokenProvider_tokenState(t *testing.T) {
+	now := time.Unix(1337, 0) // mock value for time.Now()
+
+	testCases := []struct {
+		token         *Token
+		staleDuration time.Duration
+		want          tokenState
+	}{
+		{
+			token:         nil,
+			staleDuration: 10 * time.Minute,
+			want:          expired,
+		},
+		{
+			token: &Token{
+				Expiry: now.Add(-1 * time.Second),
+			},
+			staleDuration: 10 * time.Minute,
+			want:          expired,
+		},
+		{
+			token: &Token{
+				Expiry: now.Add(1 * time.Hour),
+			},
+			staleDuration: 10 * time.Minute,
+			want:          fresh,
+		},
+		{
+			token: &Token{
+				Expiry: now.Add(5 * time.Minute),
+			},
+			staleDuration: 10 * time.Minute,
+			want:          stale,
+		},
+	}
+
+	for _, tc := range testCases {
+		cts := &cachedTokenProvider{
+			cachedToken:   tc.token,
+			staleDuration: tc.staleDuration,
+			disableAsync:  false,
+			timeNow:       func() time.Time { return now },
+		}
+
+		got := cts.tokenState()
+
+		if got != tc.want {
+			t.Errorf("tokenState() = %v, want %v", got, tc.want)
+		}
+	}
+}
+
+func TestCachedTokenProvider_Token(t *testing.T) {
+	now := time.Unix(1337, 0) // mock value for time.Now()
+	nTokenCalls := 10         // number of goroutines calling Token()
+	testCases := []struct {
+		desc         string // description of the test case
+		cachedToken  *Token // token cached before calling Token()
+		disableAsync bool   // whether are disabled or not
+		refreshErr   error  // whether the cache was in error state
+
+		returnedToken *Token // token returned by the token source
+		returnedError error  // error returned by the token source
+
+		wantCalls int    // expected number of calls to the token source
+		wantToken *Token // expected token in the cache
+	}{
+		{
+			desc:          "[Blocking] no cached token",
+			disableAsync:  true,
+			returnedToken: &Token{Expiry: now.Add(1 * time.Hour)},
+			wantCalls:     1,
+			wantToken:     &Token{Expiry: now.Add(1 * time.Hour)},
+		},
+		{
+			desc:          "[Blocking] expired cached token",
+			disableAsync:  true,
+			cachedToken:   &Token{Expiry: now.Add(-1 * time.Second)},
+			returnedToken: &Token{Expiry: now.Add(1 * time.Hour)},
+			wantCalls:     1,
+			wantToken:     &Token{Expiry: now.Add(1 * time.Hour)},
+		},
+		{
+			desc:         "[Blocking] fresh cached token",
+			disableAsync: true,
+			cachedToken:  &Token{Expiry: now.Add(1 * time.Hour)},
+			wantCalls:    0,
+			wantToken:    &Token{Expiry: now.Add(1 * time.Hour)},
+		},
+		{
+			desc:         "[Blocking] stale cached token",
+			disableAsync: true,
+			cachedToken:  &Token{Expiry: now.Add(1 * time.Minute)},
+			wantCalls:    0,
+			wantToken:    &Token{Expiry: now.Add(1 * time.Minute)},
+		},
+		{
+			desc:          "[Blocking] refresh error",
+			disableAsync:  true,
+			returnedError: fmt.Errorf("test error"),
+			wantCalls:     10,
+		},
+		{
+			desc:          "[Blocking] recover from error",
+			disableAsync:  true,
+			refreshErr:    fmt.Errorf("refresh error"),
+			cachedToken:   &Token{Expiry: now.Add(-1 * time.Minute)},
+			returnedToken: &Token{Expiry: now.Add(-1 * time.Hour)},
+			wantCalls:     10,
+			wantToken:     &Token{Expiry: now.Add(-1 * time.Hour)},
+		},
+		{
+			desc:          "[Async] no cached token",
+			returnedToken: &Token{Expiry: now.Add(1 * time.Hour)},
+			wantCalls:     1,
+			wantToken:     &Token{Expiry: now.Add(1 * time.Hour)},
+		},
+		{
+			desc:          "[Async] no cached token",
+			returnedToken: &Token{Expiry: now.Add(1 * time.Hour)},
+			wantCalls:     1,
+			wantToken:     &Token{Expiry: now.Add(1 * time.Hour)},
+		},
+		{
+			desc:          "[Async] expired cached token",
+			cachedToken:   &Token{Expiry: now.Add(-1 * time.Second)},
+			returnedToken: &Token{Expiry: now.Add(1 * time.Hour)},
+			wantCalls:     1,
+			wantToken:     &Token{Expiry: now.Add(1 * time.Hour)},
+		},
+		{
+			desc:        "[Async] fresh cached token",
+			cachedToken: &Token{Expiry: now.Add(1 * time.Hour)},
+			wantCalls:   0,
+			wantToken:   &Token{Expiry: now.Add(1 * time.Hour)},
+		},
+		{
+			desc:          "[Async] stale cached token",
+			cachedToken:   &Token{Expiry: now.Add(1 * time.Minute)},
+			returnedToken: &Token{Expiry: now.Add(1 * time.Hour)},
+			wantCalls:     1,
+			wantToken:     &Token{Expiry: now.Add(1 * time.Hour)},
+		},
+		{
+			desc:          "[Async] refresh error",
+			cachedToken:   &Token{Expiry: now.Add(1 * time.Minute)},
+			returnedError: fmt.Errorf("test error"),
+			wantCalls:     1,
+			wantToken:     &Token{Expiry: now.Add(1 * time.Minute)},
+		},
+		{
+			desc:          "[Async] stale cached token, expired token returned",
+			cachedToken:   &Token{Expiry: now.Add(1 * time.Minute)},
+			returnedToken: &Token{Expiry: now.Add(-1 * time.Second)},
+			wantCalls:     1,
+			wantToken:     &Token{Expiry: now.Add(-1 * time.Second)},
+		},
+		{
+			desc:          "[Async] recover from error",
+			refreshErr:    fmt.Errorf("refresh error"),
+			cachedToken:   &Token{Expiry: now.Add(-1 * time.Minute)},
+			returnedToken: &Token{Expiry: now.Add(-1 * time.Hour)},
+			wantCalls:     10,
+			wantToken:     &Token{Expiry: now.Add(-1 * time.Hour)},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			gotCalls := int32(0)
+			cts := &cachedTokenProvider{
+				disableAsync:  tc.disableAsync,
+				staleDuration: 10 * time.Minute,
+				cachedToken:   tc.cachedToken,
+				timeNow:       func() time.Time { return now },
+				TokenProvider: TokenProviderFn(func(_ context.Context) (*Token, error) {
+					atomic.AddInt32(&gotCalls, 1)
+					time.Sleep(10 * time.Millisecond)
+					return tc.returnedToken, tc.returnedError
+				}),
+			}
+
+			wg := sync.WaitGroup{}
+			for i := 0; i < nTokenCalls; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					cts.Token(context.Background())
+				}()
+			}
+
+			wg.Wait()
+
+			// Wait for async refreshes to finish. This part is a little brittle
+			// but necessary to ensure that the async refresh is done before
+			// checking the results.
+			time.Sleep(20 * time.Millisecond)
+
+			if int(gotCalls) != tc.wantCalls {
+				t.Errorf("want %d calls to cts.TokenProvider.Token(), got %d", tc.wantCalls, gotCalls)
+			}
+			if !reflect.DeepEqual(tc.wantToken, cts.cachedToken) {
+				t.Errorf("want cached token %v, got %v", tc.wantToken, cts.cachedToken)
+			}
+		})
+	}
+}
---
+++ databricks/go.mod
@@ -0,0 +1,3 @@
+module github.com/databricks/sdk-go/databricks
+
+go 1.25.4
