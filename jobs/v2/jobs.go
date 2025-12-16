ac43c2b885f92b3a7cd0b097b028ed7c0094ac2a
Renaud Hartert <renaud.hartert@databricks.com>
2025-12-16 16:23:35 +0100

    Add dummy Jobs v2 client. (#11)
    ## What changes are proposed in this pull request?
    
    This PR adds a dummy Jobs v2 client. The code is meant as a way to test
    current assumption and packages while providing an example for future
    code generation.
    
    Design decisions:
    
    - `unstable` package: Having `ClientOptions` in `internal` provides
    great abstraction, preventing SDK client for accessing and modifying the
    internal client config representation. However, it also prevents the API
    client to access the logger that was already resolved when building the
    HTTP client. Package `unstable` provide a small utility to resolve the
    logger from a set of options. It works for now but we might want to
    consider alternatives (e.g. have `ClientOptions` in `unstable`).
    - `genhelpers.go`: Most generated code is expected to go through the
    same logic to send a request, log request/response, and parse errors.
    The `genhelpers.go` file is meant to be a static file containing all
    these helpers.
    - Lazy loggers: The new `slog` package provides utility to lazily
    generate logging entry. We will leverage these to log request/response
    if the logger is configured at the right level.
    
    ## How is this tested?
    
    No added test.

---
+++ jobs/v2/jobs.go
@@ -0,0 +1,112 @@
+package jobs
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"iter"
+	"log/slog"
+	"net/http"
+	"net/url"
+
+	"github.com/databricks/sdk-go/databricks/api"
+	"github.com/databricks/sdk-go/databricks/options"
+	"github.com/databricks/sdk-go/databricks/options/unstable"
+	"github.com/databricks/sdk-go/databricks/transport"
+)
+
+type Client struct {
+	httpClient *http.Client
+	logger     *slog.Logger
+	host       string
+}
+
+func NewClient(ctx context.Context, opts ...options.ClientOption) (*Client, error) {
+	resolved, err := unstable.Resolve(opts...)
+	if err != nil {
+		return nil, err
+	}
+	httpClient, err := transport.NewHTTPClient(ctx, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		httpClient: httpClient,
+		logger:     resolved.Logger,
+	}, nil
+}
+
+func (c *Client) CreateJob(ctx context.Context, req *CreateJobRequest, opts ...api.Option) (*Job, error) {
+	body, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	headers := http.Header{}
+	headers.Set("Content-Type", "application/json")
+
+	baseURL, err := url.Parse(c.host)
+	if err != nil {
+		return nil, err
+	}
+	queryParams := url.Values{}
+	baseURL.RawQuery = queryParams.Encode()
+
+	resp := &Job{}
+	call := func(ctx context.Context) error {
+		httpReq, err := http.NewRequest("POST", baseURL.String(), bytes.NewBuffer(body))
+		if err != nil {
+			return err
+		}
+		httpReq = httpReq.WithContext(ctx)
+		httpReq.Header = headers
+
+		respBody, err := executeHTTPCall(httpCallOptions{
+			req:    httpReq,
+			client: c.httpClient,
+			logger: c.logger,
+		})
+		if err != nil {
+			return err
+		}
+		if err := json.Unmarshal(respBody, resp); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := api.Execute(ctx, call, opts...); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func ListJobs(ctx context.Context, req *ListJobsRequest, opts ...api.Option) (*ListJobsResponse, error) {
+	return nil, nil
+}
+
+func ListJobsIter(ctx context.Context, req *ListJobsRequest, opts ...api.Option) iter.Seq2[*Job, error] {
+	return func(yield func(*Job, error) bool) {
+		pageReq := &ListJobsRequest{
+			PageSize:  req.PageSize,
+			PageToken: req.PageToken,
+		}
+		for {
+			resp, err := ListJobs(ctx, pageReq, opts...)
+			if err != nil {
+				yield(nil, err)
+				return
+			}
+			for i := range resp.Jobs {
+				if !yield(&resp.Jobs[i], nil) {
+					return
+				}
+			}
+			if resp.NextPageToken == "" {
+				return
+			}
+			pageReq.PageToken = resp.NextPageToken
+		}
+	}
+}
